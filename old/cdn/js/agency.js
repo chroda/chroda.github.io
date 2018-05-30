091406970da99f586181e4322a86a74498df3f22
chroda <chroda@chroda.com.br>
2018-05-29 21:25:19 -0300

    old

---
+++ old/cdn/js/agency.js
@@ -0,0 +1,81 @@
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+$("img").lazyload({
+    effect : "fadeIn"
+});
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+function preload(srcArray,dir,subdir) {
+  for(var i = 0; i < srcArray.length; i++) {
+    (new Image()).src = srcArray[i];
+  }
+}
+$(window).load( function(){
+  var dir = "./cdn/img/";
+  preload([
+    dir+'header-bg.jpg',
+    dir+'map-image.png',
+    dir+'logos/logo_black.svg',
+    // dir+'logos/logo_red_spacing.svg',
+    dir+'logos/logo_red.svg',
+    dir+'logos/logo_white.svg',
+    dir+'portfolio/formerjob_grupow.png',
+    dir+'portfolio/formerjob_grupow_modal.png',
+    dir+'portfolio/formerjob_softmak.png',
+    dir+'portfolio/formerjob_softmak_modal.png',
+    dir+'portfolio/formerjob_viaartistica.png',
+    dir+'team/chroda.jpeg',
+  ]);
+});
+
+function skillProgress(skillId,skillValue){
+  var skillProgressOptions = {
+    color: '#fff',
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 2000,
+    from: { color: '#C0262C', width:4},
+    to: { color: '#C0262C', width:4},
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+      var value = Math.round(circle.value() * 100);
+      value === 0 ? circle.setText('') : circle.setText(skillId.getAttribute('id') + ' ' +value + '%');
+    }
+  };
+  var bar = new ProgressBar.Circle(skillId,skillProgressOptions);
+  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+  bar.text.style.fontSize = '1.2rem';
+  bar.animate(skillValue);
+}
+
+skillProgress(php,.83);
+skillProgress(html5,.92);
+skillProgress(css3,.91);
+skillProgress(mysql,.6);
+skillProgress(mongodb,.7);
+skillProgress(firebase,.5);
+skillProgress(git,.75);
+skillProgress(linux,.58);
+skillProgress(js,.72);
+skillProgress(nodejs,.5);
+skillProgress(jquery,.85);
+skillProgress(angular,.5);
+skillProgress(vue,.4);
+skillProgress(react,.2);
+skillProgress(ionic,.41);
+skillProgress(express,.1);
+skillProgress(sass,.49);
---
+++ old/cv.md
@@ -0,0 +1,56 @@
+# Christian Marcell Oliveira
+Web Developer
+<br><small id="age">28 years old - Single</small>
+
+<div id="webaddress">
+<i class="fa fa-phone"></i> <a href="tel:+5547991443264">+55 47 99144-3264</a>
+|
+<a href="mailto:chroda@chroda.com.br">chroda@chroda.com.br</a>
+|
+<a href="http://chroda.com.br">http://chroda.com.br</a>
+|
+<a href="https://github.com/chroda"><i class="fa fa-github"></i> https://github.com/chroda</a>
+</div>
+
+<br />
+
+## About
+
+I am a web developer since 2010, located in Florianópolis, Brazil.
+Most of my personal skills that I can point are the curiosity,
+the organization on coding, the seek for self improvement.
+I have experience in developing mid to large-scale websites and web-applications based on webstandard technologies like PHP, HTML5, CSS3, JavaScript.
+
+## Expertise & Core Skills
+
+### Development:
+- PHP
+- HTML5
+- CSS3
+- Javascript (AngularJS/React/jQuery)
+- MVC
+- Design Patterns
+- MySQL
+- Git
+- Responsive Development
+- Performance optimization
+
+### Tooling / Workflow:
+- Git: Git Flow, GitHub Flow
+- Package Management: npm, git
+
+## Work History
+
+- ViaArtistica - Fullstack Developer - portal website and ERP system.
+- SoftMak - Fullstack Developer - custom system.
+- ViaArtistica - Fullstack Developer - sites and online systems.
+
+## Language<!---->
+
+- ** Portuguese ** - My native Language.
+- ** English ** - Intermediate understanding.
+
+## Links
+
+* <i class="fa fa-globe"></i> <a href="http://chroda.com.br"><strong>Website:</strong> chroda.com.br</a><br />
+* <i class="fa fa-github"></i> <a href="http://github.com/chroda"><strong>Github:</strong> github.com/chroda</a><br />
